fee016086b6cc8da71149e450a194d591dc4cff1
Priyanshu Kumawat <priyanshukumawat434@gmail.com>
2024-01-09 14:48:08 +0530

    add complete user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,13 @@
 import express from "express";
-import userLogin from "../controllers/login.controller.js";
+import { createSubordinate, deleteSubordinate, fetchInfo, fetchSubordinates } from "../controllers/user.controller.js";
+import checkRole from "../middleware/rbac.middleware.js";
+import Roles from "../config/roles.js";
 
 const router = express.Router();
 
-router.post('/', userLogin);
-
-router.get('/self');
-router.get('/list');
-router.post('/create');
-router.delete('/delete/:userId');
+router.get('/self', fetchInfo);
+router.get('/list', checkRole([Roles.SuperAdmin, Roles.BranchManager]), fetchSubordinates);
+router.post('/create', checkRole([Roles.SuperAdmin, Roles.BranchManager]), createSubordinate);
+router.delete('/delete/:userId', checkRole([Roles.SuperAdmin, Roles.BranchManager]), deleteSubordinate);
 
 export default router
